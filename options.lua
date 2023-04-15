f9d58bef24dbd90d3a01e9cf494ae1d62488a9a2
pojok code <pojokcodeid@gmail.com>
2023-04-15 17:28:06 +0700

    update

--- options.lua
+++ options.lua
@@ -1,29 +1,55 @@
--- set vim options here (vim.<first_key>.<second_key> = value)
-return {
-  opt = {
-    -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = true, -- sets vim.opt.wrap
-  },
-  g = {
-    mapleader = " ", -- sets vim.g.mapleader
-    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true, -- enable completion at start
-    autopairs_enabled = true, -- enable autopairs at start
-    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-    ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-  },
+local options = {
+  backspace = vim.opt.backspace + { "nostop" }, -- Don't stop backspace at insert
+  clipboard = "unnamedplus", -- Connection to the system clipboard
+  cmdheight = 0, -- hide command line unless needed
+  completeopt = { "menuone", "noselect" }, -- Options for insert mode completion
+  copyindent = true, -- Copy the previous indentation on autoindenting
+  cursorline = true, -- Highlight the text line of the cursor
+  expandtab = true, -- Enable the use of space in tab
+  fileencoding = "utf-8", -- File content encoding for the buffer
+  fillchars = { eob = " " }, -- Disable `~` on nonexistent lines
+  history = 100, -- Number of commands to remember in a history table
+  ignorecase = true, -- Case insensitive searching
+  laststatus = 3, -- globalstatus
+  lazyredraw = true, -- lazily redraw screen
+  mouse = "a", -- Enable mouse support
+  number = true, -- Show numberline
+  preserveindent = true, -- Preserve indent structure as much as possible
+  pumheight = 10, -- Height of the pop up menu
+  relativenumber = true, -- Show relative numberline
+  scrolloff = 8, -- Number of lines to keep above and below the cursor
+  shiftwidth = 2, -- Number of space inserted for indentation
+  showmode = false, -- Disable showing modes in command line
+  showtabline = 2, -- always display tabline
+  sidescrolloff = 8, -- Number of columns to keep at the sides of the cursor
+  signcolumn = "yes", -- Always show the sign column
+  smartcase = true, -- Case sensitivie searching
+  splitbelow = true, -- Splitting a new window below the current one
+  splitright = true, -- Splitting a new window at the right of the current one
+  swapfile = false, -- Disable use of swapfile for the buffer
+  tabstop = 2, -- Number of space in a tab
+  termguicolors = true, -- Enable 24-bit RGB color in the TUI
+  timeoutlen = 300, -- Length of time to wait for a mapped sequence
+  undofile = true, -- Enable persistent undo
+  updatetime = 300, -- Length of time to wait before triggering the plugin
+  wrap = true, -- Disable wrapping of lines longer than the width of window
+  writebackup = false, -- Disable making a backup before overwriting a file
+  -- minimal number of screen columns either side of cursor if wrap is `false`
+  -- guifont = "monospace:h17", -- the font used in graphical neovim applications
+  guifont = "Source Code Pro:h17", -- the font used in graphical neovim applications
+  -- guifont = "Hack Nerd Font:h17", -- the font used in graphical neovim applications
+  whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line            -- which "horizontal" keys are allowed to travel to prev/next line
 }
--- If you need more control, you can use the function()...end notation
--- return function(local_vim)
---   local_vim.opt.relativenumber = true
---   local_vim.g.mapleader = " "
---   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
---   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
---   return local_vim
--- end
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+-- vim.opt.shortmess = "ilmnrx"                        -- flags to shorten vim messages, see :help 'shortmess'
+-- vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+vim.opt.shortmess:append "c" -- don't give |ins-completion-menu| messages
+vim.opt.iskeyword:append "-" -- hyphenated words recognized by searches
+vim.opt.formatoptions:remove { "t", "c", "q", "j" }
+vim.opt.formatoptions = "croql"
+-- vim.opt.formatoptions:remove({ "c", "r", "o" }) -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
+vim.opt.runtimepath:remove "/usr/share/vim/vimfiles" -- separate vim plugins from neovim in case vim still in use
